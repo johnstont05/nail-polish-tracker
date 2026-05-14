059e0cb84927678bbaf1ffa838275bb07f6bdd2c
johnstont05 <taylorfjohnston@gmail.com>
2026-05-14 15:31:56 -0400

    netlify assets

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/nail-polish-tracker/',
   resolve: {
     alias: {
       $lib: path.resolve(__dirname, './src/lib'),
